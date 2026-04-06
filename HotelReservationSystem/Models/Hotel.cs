3ca89f4cf2074e98f864f05a0f5cff7c6617a1f5
Dani1723 <danigroza17@gmail.com>
2026-04-06 11:13:26 +0300

    Adăugat modelele de date: Hotel, Camera, Client

---
+++ HotelReservationSystem/Models/Hotel.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelReservationSystem.Models
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Nume { get; set; }
+        public string Locatie { get; set; }
+        public string Descriere { get; set; }
+        public string Facilitati { get; set; }
+
+        // Aceasta este o listă de obiecte de tip Camera
+        public List<Camera> Camere { get; set; } = new List<Camera>();
+    }
+}
---
+++ HotelReservationSystem/Models/Plata.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelReservationSystem.Models
+{
+    public class Plata
+    {
+        public int Id { get; set; }
+        public int RezervareId { get; set; }
+
+        public decimal Suma { get; set; }
+        public string Metoda { get; set; } // Card, Cash, Voucher
+        public DateTime DataPlatii { get; set; } = DateTime.Now;
+
+        public Rezervare Rezervare { get; set; }
+    }
+}
