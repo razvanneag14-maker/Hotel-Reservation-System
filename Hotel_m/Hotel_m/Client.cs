05a9bef8a330ef443fd8c9833abdf222767341b3
Dani1723 <danigroza17@gmail.com>
2026-04-18 18:10:48 +0300

    Am facut metode la clase

--- Hotel_m/Hotel_m/Client.cs
+++ Hotel_m/Hotel_m/Client.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Hotel_m
 {
     internal class Client
     {
+        public string Nume { get; set; } // Adăugat pentru context
         public int Varsta { get; set; }
         public string Email { get; set; }
         public string NrTelefon { get; set; }
         public List<Rezervare> Rezervari { get; set; }
 
-        public Client(int varsta, string email, string nrTelefon)
+        public Client(string nume, int varsta, string email, string nrTelefon)
         {
+            Nume = nume;
             Varsta = varsta;
             Email = email;
             NrTelefon = nrTelefon;
@@ -19,17 +22,42 @@ namespace Hotel_m
 
         public void FaRezervare(Rezervare rezervare)
         {
-            Rezervari.Add(rezervare);
+            // Verificăm dacă rezervarea nu e nulă înainte de a o adăuga
+            if (rezervare != null)
+            {
+                Rezervari.Add(rezervare);
+                Console.WriteLine($"Rezervarea pentru {Nume} a fost înregistrată.");
+            }
         }
 
         public void AnuleazaRezervare(Rezervare rezervare)
         {
-            Rezervari.Remove(rezervare);
+            if (Rezervari.Contains(rezervare))
+            {
+                Rezervari.Remove(rezervare);
+                Console.WriteLine("Rezervarea a fost anulată cu succes.");
+            }
         }
 
+        // Implementarea metodei din diagrama pentru Recenzii
         public void LasaRecenzie(Hotel hotel, string comentariu, int rating)
         {
-            
+            // Creăm un obiect nou de tip Recenzie folosind clasa făcută anterior
+            Recenzie nouaRecenzie = new Recenzie(this, hotel, rating, comentariu);
+
+            // Adăugăm recenzia în lista hotelului (presupunând că Hotel are o List<Recenzie>)
+            hotel.Recenzii.Add(nouaRecenzie);
+
+            Console.WriteLine($"Clientul {Nume} a lăsat o recenzie de {rating} stele pentru hotelul {hotel.Locatie}.");
+        }
+
+        public void StergeRecenzie(Hotel hotel, Recenzie recenzie)
+        {
+            if (hotel.Recenzii.Contains(recenzie))
+            {
+                hotel.Recenzii.Remove(recenzie);
+                Console.WriteLine("Recenzia a fost ștearsă.");
+            }
         }
     }
 }
